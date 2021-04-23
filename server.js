bbdb3ae650db0b1c588018f96d1caf4ffadc7607
Ivan Kryvenko <ivan.kryv14@gmail.com>
2021-04-23 22:48:01 +0300

    v1.0.1

--- server.js
+++ server.js
@@ -12,6 +12,15 @@ app.get('/', (req, res) => {
     res.json({ message: 'Start page' });
 });
 
+require('./app/routes/archive.router')(app);
+require('./app/routes/city.router')(app);
+require('./app/routes/client.router')(app);
+require('./app/routes/employee.router')(app);
+require('./app/routes/posting.router')(app);
+require('./app/routes/postOffice.router')(app);
+require('./app/routes/service.router')(app);
+require('./app/routes/transport.router')(app);
+
 app.listen(port, () => {
     console.log(`server listening in ${port} port`);
 })
